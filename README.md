ec219892bc607db9969a49a7335ca7152d50938e
Thien-Bach Huynh <tbhuynh@mail.usf.edu>
2018-03-03 12:56:36 -0800

    adding post maker
    adding script to fetch raw markdown and save them as posts.

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # rocks
 Landing page for snail.rocks
 
-## Requirements
-To get started with jekyll: [https://jekyllrb.com/docs/installation](Installation) or [https://jekyllrb.com/docs/windows/](Installation on Windows)
+## Installation
+*Non-windows users*
+  [https://jekyllrb.com/docs/installation](Installation)
+
+*Windows users* 
+[https://jekyllrb.com/docs/windows/](Installation on Windows)
+Note: if you have trouble with building `nokogiri` via `bundle update` or `bundle install` try doing `gem install nokogiri --no-ri` and then run `bundle <update|install>` again 
 
 Usage:  https://jekyllrb.com/docs/usage/
 
@@ -19,3 +24,9 @@ Username for different services such as github and twitter are specified in
 - Icons: [https://github.com/primer/octicons/tree/master/lib/jekyll-octicons](octicons)
 
 The repo is configured such that `gh-pages` is treated like master.
+
+## Testing
+`bundle exec rspec post_maker_spec.rb`
+
+## Deployment
+Push to `gh-pages` to deploy.
--- post_maker.rb
+++ post_maker.rb
@@ -2,13 +2,64 @@
 # gets the README.md file from the listed projects
 # and outputs a jekyll-aware `YEAR-MONTH-DAY-title.md` file in the `_posts` directory.
 
-# TODO: 1. append `/master/README.md` to end of url
-# 2. prepend 'https://raw.githubusercontent.com' to beginning of url
-# 3. prepend 'https://github.com/' to beginning of url to get github repo link
 require 'yaml'
-FILENAME = 'project_list.yml'
+require 'uri'
+require 'net/http'
+require 'pathname'
 
-def load_yaml
-    YAML.load_file(FILENAME)
+class PostMaker
+    attr_reader :site_list
+    def initialize(posts_folder=Pathname('_posts'), filename='project_list.yml', base_uri='https://raw.githubusercontent.com')
+        @filename = filename
+        @site_list = YAML.load_file(@filename)
+        @base_uri = base_uri
+        @posts_folder = posts_folder 
+    end
+
+    def extract_path(uri)
+        URI::parse(uri).path
+    end
+
+    def build_url(uri)
+        path = extract_path(uri)
+        URI::parse(@base_uri + path)
+    end
+
+    def get_project_name(path)
+        path.split('/').last
+    end
+
+    def fetch(uri)
+        res = Net::HTTP.get_response(uri)
+        res.body
+    end
+
+    def post_exists?(project_name)
+        @posts_folder.children.any? { |post_file| post_file.to_s.include? project_name }
+    end
+
+    def build_post_title(project_name)
+        
+    end
+
+    def write_markdown_to_post(markdown)
+    end
+
+    def build_posts_from_yaml(site_url)
+        project_name = get_project_name(site_url)
+        return if project_exists?(project_name)
+        markdown_url = build_url(extract_path(site_url))
+        markdown = fetch(markdown_url)
+    end
+
+    def build_posts
+        @site_list.each do |site_url|
+            build_posts_from_yaml(site_url)
+        end
+    end
 end
 
+if __FILE__ == $0
+    post_maker = PostMaker.new
+    post_maker.build_posts
+end
